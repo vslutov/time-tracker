05da7acb9e5601b5ff7fe60793d952a3c3aef4b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-17 14:37:17 +0000

    Deploy vslutov/time-tracker to github.com/vslutov/time-tracker.git:gh-pages

---
+++ precache-manifest.05477ba578c74ada25164a409dd11437.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd16456fa26fa55b3d0366a3c9c724e7",
+    "url": "/time-tracker/index.html"
+  },
+  {
+    "revision": "3639b598a9b0189875b2",
+    "url": "/time-tracker/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b5f5dfd677e1169ab219",
+    "url": "/time-tracker/static/css/main.355b6af9.chunk.css"
+  },
+  {
+    "revision": "3639b598a9b0189875b2",
+    "url": "/time-tracker/static/js/2.407c1522.chunk.js"
+  },
+  {
+    "revision": "b5f5dfd677e1169ab219",
+    "url": "/time-tracker/static/js/main.344e6935.chunk.js"
+  },
+  {
+    "revision": "7df5507a1c30fc8b3d5e",
+    "url": "/time-tracker/static/js/runtime~main.ee8c59cf.js"
+  }
+]);
